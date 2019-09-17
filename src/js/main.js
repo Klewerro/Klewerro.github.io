9ce8167da588721f0c400b3e11ca12a2a44645ff
PSzymon <pol.szym95@gmail.com>
2019-09-17 20:21:47 +0100

    Last commit message added on hover at updated date.
    Added fetch, counter and alert, after GitHub API request limit reached.

--- src/js/main.js
+++ src/js/main.js
@@ -6,18 +6,30 @@ fetch('https://api.github.com/users/klewerro/repos?sort=pushed&direction=desc')
     .then(response => response.json())
     .then(json => {
         const repos = json;
+
         for (const repo of repos.slice(0, 6)) {
-            projectsContainer.innerHTML += repoToHtml(repo);
+            const { name, pushed_at } = repo;
+            let lastCommitMessage;
+            fetch(`https://api.github.com/repos/klewerro/${name}/commits`)
+                .then(response => response.json())
+                .then(json => lastCommitMessage = json[0].commit.message)
+                .then(() =>  projectsContainer.innerHTML += repoToHtml(repo, lastCommitMessage));
         }
-    }).catch(err => console.log(err));
+
+        checkNumberOfRemainingRequests();
+    })
+    .catch(err => {
+        console.log(err);
+        checkNumberOfRemainingRequests();
+    });
 
 
-function repoToHtml(repo) {
+function repoToHtml(repo, lastCommitMessage) {
     const { description, name, homepage, html_url, pushed_at } = repo;
-
+    
     return ` 
     <article class="project">
-    <p class="project__updated">Updated: ${new Date(pushed_at).toLocaleDateString()}</p>
+    <p class="project__updated" title="${lastCommitMessage}">Updated: ${new Date(pushed_at).toLocaleDateString()}</p>
     <section class="project__content">
       <img class="project__image" src="assets/img/github.svg" />
       <h3 class="project__title">${name}</h3>
@@ -32,4 +44,21 @@ function repoToHtml(repo) {
       <a class="project__link project__link--code" title="Source code: ${name}." target="_blank" rel="nofollow noreferrer" href="${html_url}">Github</a>
     </footer>
   </article>`;
+}
+
+function checkNumberOfRemainingRequests() {
+    fetch('https://api.github.com/rate_limit')
+    .then(response => response.json())
+    .then(json => {
+        const {limit, remaining} = json.rate;
+        if (remaining > 0) {
+            console.log("Remaining GitHub API requests for your IP address: ", 
+            `${remaining}/${limit}`);
+        } else {
+            const message = "You have reached Github API 60 request per hour limit from your IP address. Unfortunatelly recent projects will be not available until reset."
+            console.log(message);
+            alert(message);
+        }
+        
+    });
 }
